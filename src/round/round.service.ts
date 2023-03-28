fc6871e16f55e0780e1fab6efee9dfa1d6bce81e
podnesTaF <r0934777@student.thomasmore.be>
2023-03-28 22:36:33 +0200

    adapt to sockets frontend

--- src/round/round.service.ts
+++ src/round/round.service.ts
@@ -45,7 +45,10 @@ export class RoundService {
     return qb.getMany()
   }
 
-  findOne(id: number) {
+  findOne(id: number, q: string | undefined) {
+    if(q) {
+      return this.repository.findOne({where: {game: {id}, round_status: 'active'},  relations: ['moves', 'riddler', 'game']})
+    }
     return this.repository.findOne({where: {id}, relations: ['moves', 'riddler', 'game']})
   }
 
